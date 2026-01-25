503a7053fd664b1653618ad071504d4f780c5d6d
Tia Sajeev <tsajeev@students.cs.ubc.ca>
2026-01-24 23:04:39 -0800

    made everything responsive + added footer to main

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,14 @@ import './App.css'
 import Hero from './sections/Hero/Hero';
 import Projects from './sections/Projects/Projects';
 import Contact from './sections/Contact/Contact';
-
+import Footer  from './sections/Footer/Footer.jsx';
 function App() {
   return (
     <>
       <Hero/>
       <Projects/>
       <Contact/>
+      <Footer/>
     </>
   )
 }
